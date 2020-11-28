dca97687e536f6698833de9a4965507568092c46
Nepra <neprimerovami@gmail.com>
2020-11-28 22:51:26 +0300

    ITERATOR-1022 Added code

--- iterator/iterator.py
+++ iterator/iterator.py
@@ -1,39 +1,102 @@
+class Node:
+    def __init__(self, val, next=None, prev=None):
+        self.val = val
+
+        if next != None:
+            self.next = next
+
+        if prev != None:
+            self.prev = prev
+
+
 class MyLinkedList:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
+        self.head = None
+
+    def len(self):
+        if self.head == None:
+            return 0
+        cur = self.head
+        res = 1
+        cur = cur.next
+        while cur != self.head:
+            cur = cur.next
+            res += 1
+        return res
+
+    def getNode(self, index: int) -> Node:
+        cur = self.head
+        if self.head == None:
+            return None
+        for i in range(index):
+            if cur.next == self.head:
+                return None
+            cur = cur.next
+        return cur
 
     def get(self, index: int) -> int:
-        """
-        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
-        """
+        result = self.getNode(index)
+        if result == None:
+            return -1
+        return result.val
 
     def addAtHead(self, val: int) -> None:
-        """
-        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
-        """
+        node = Node(val)
+        if self.head == None:
+            self.head = node
+            node.next = node
+            node.prev = node
+        else:
+            node.next = self.head
+            node.prev = self.head.prev
+            node.next.prev = node
+            node.prev.next = node
+            self.head = node
 
     def addAtTail(self, val: int) -> None:
-        """
-        Append a node of value val to the last element of the linked list.
-        """
+        if self.head == None:
+            self.addAtHead(val)
+            return
+        node = Node(val)
+        node.prev = self.head.prev
+        node.next = self.head
+        node.prev.next = node
+        node.next.prev = node
 
     def addAtIndex(self, index: int, val: int) -> None:
-        """
-        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
-        """
+        if index == self.len():
+            self.addAtTail(val)
+            return
+        if index == 0:
+            self.addAtHead(val)
+            return
+        indexth = self.getNode(index)
+        if indexth == None:
+            return
+        node = Node(val)
+        node.prev = indexth.prev
+        node.next = indexth
+        node.next.prev = node
+        node.prev.next = node
 
     def deleteAtIndex(self, index: int) -> None:
-        """
-        Delete the index-th node in the linked list, if the index is valid.
-        """
+        indexth = self.getNode(index)
+        print(1)
+        if indexth == None:
+            return
+        indexth.prev.next = indexth.next
+        indexth.next.prev = indexth.prev
+        if index == 0:
+            self.head = indexth.next
 
-# Your MyLinkedList object will be instantiated and called as such:
-# obj = MyLinkedList()
-# param_1 = obj.get(index)
-# obj.addAtHead(val)
-# obj.addAtTail(val)
-# obj.addAtIndex(index,val)
-# obj.deleteAtIndex(index)
+    def print(self):
+        if self.head == None:
+            return
+        cur = self.head
+        res = [cur.val]
+        cur = cur.next
+        while cur != self.head:
+            res.append(cur.val)
+            cur = cur.next
+        print(res)
